17f589eb8ba6a6ae29647f8797669df6fe118523
luisalfredomtz <luisalfredomtz@hotmail.com>
2023-11-06 03:01:12 -0600

    Fix: show title

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -2,14 +2,15 @@
   <header class="bg-nepal-300 p-4 px-24 flex items-center justify-between">
     <div class="flex items-center space-x-2 h-7">
       <img :src="useAsset(logo)" alt="meddie logo" class="w-20" />
-      <h1 v-if="title && !!isAuth" class="text-black text-lg font-semibold">
+      <h1 v-if="showTitle" class="text-black text-lg font-semibold">
         {{ title }}
       </h1>
     </div>
-    <nav v-if="!!isAuth" class="space-x-4">
+    <nav v-if="showTitle" class="space-x-4">
       <a v-for="(item, index) in navigationLinks" :key="index" :href="item.link" class="text-black hover:underline">
         {{ item.text }}
       </a>
+      <button @click="logUserOut" class="font-bold">Cerrar sesión</button>
     </nav>
   </header>
 </template>
@@ -36,11 +37,18 @@ export default {
     definePageMeta({
       middleware: 'auth'
     })
-    const { logUserIn, loading, authenticated } = useAuthStore();
+    const authStore  = useAuthStore();
+
+
+    const showTitle = computed(() => {
+      return authStore.authenticated;
+    });
 
     return {
-      isAuth: authenticated,
-      loading,
+      showTitle,
+      logUserOut: authStore.logUserOut,
+      isAuth: authStore.authenticated,
+      loading: authStore.loading,
       navigationLinks: [
         { text: "Inicio", link: "/" },
         { text: "Acerca", link: "/acerca" },
